cf1430e0af4ca059f78c202837baae5b99c66e84
CorentinDupont <dupont.corentin44@gmail.com>
2018-11-05 14:29:52 +0100

    all json files, UserController

--- users/UserController.js
+++ users/UserController.js
@@ -1,33 +1,113 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-// CREATE A NEW USER
-router.post('/new', function (req, res) {
+const ENTITY_NAME = "user";
+const ENTITY_NAME_PLURAL = "users";
+const ENTITY_FILE_PATH = "users/users.json"
 
+// CREATE
+router.post('/', function (req, response) {
+
+  fs.readFile(ENTITY_FILE_PATH, 'utf8', (err, data) => {
+    if (err){
+        console.log(err);
+    } else {  
+
+      let obj = JSON.parse(data);
+      const newObject = {id:obj[ENTITY_NAME_PLURAL].length+1, name:req.body.name, password:req.body.password}
+      obj[ENTITY_NAME_PLURAL].push(newObject);
+
+      console.log(obj[ENTITY_NAME_PLURAL]);
+
+      json = JSON.stringify(obj);
+      fs.writeFile(ENTITY_FILE_PATH, json, 'utf8', (err, res) => {
+        if(err){
+          console.error(err);
+        } else {
+          response.status(200).send(`${ENTITY_NAME} ${JSON.stringify(newObject)} has been successfully registered !`);
+        }
+      });
+    }
+  });
 });
 
-// GETS ALL USERS
-router.get('/', function (req, res) {
-  
+// GET ONE BY ID
+router.get('/:id', function (req, response) {
+  fs.readFile(ENTITY_FILE_PATH, 'utf8', (err, data) => {
+    if (err){
+        console.log(err);
+    } else {  
+      const obj = JSON.parse(data);
+      const index = obj[ENTITY_NAME_PLURAL].findIndex(el=> (el.id === Number(req.params.id)));
+      const selectedObject = obj[ENTITY_NAME_PLURAL][index];
+      response.status(200).send(selectedObject);
+    }
+  });
 });
 
+// DELETE BY ID
+router.delete('/:id', function (req, response) {
+  fs.readFile(ENTITY_FILE_PATH, 'utf8', (err, data) => {
+    if (err){
+        console.log(err);
+    } else {  
+      const obj = JSON.parse(data);
+      const index = obj[ENTITY_NAME_PLURAL].findIndex(el=> (el.id === Number(req.params.id)));
+      const removedObject = obj[ENTITY_NAME_PLURAL][index];
+      obj[ENTITY_NAME_PLURAL].splice(index, 1);
 
-// GET ONE USER BY ID
-router.get('/:id', function (req, res) {
-
+      json = JSON.stringify(obj);
+      fs.writeFile(ENTITY_FILE_PATH, json, 'utf8', (err, res) => {
+        if(err){
+          console.error(err);
+        } else {
+          response.status(200).send(`${ENTITY_NAME} ${JSON.stringify(removedObject)} has been successfully deleted !`);
+        }
+      });
+    }
+  });
 });
 
-// DELETE A USER BY ID
-router.delete('/:id', function (req, res) {
+// UPDATE ONE BY ID
+router.put('/:id', function (req, response) {
+  fs.readFile(ENTITY_FILE_PATH, 'utf8', (err, data) => {
+    if (err){
+        console.log(err);
+    } else {  
+      const obj = JSON.parse(data);
+      const index = obj[ENTITY_NAME_PLURAL].findIndex(el=> (el.id === Number(req.params.id)));
+      const objectToUpdate = obj[ENTITY_NAME_PLURAL][index];
+      const updatedObject =  {id:req.params.id, name:req.body.name, password:req.body.password};
+      obj[ENTITY_NAME_PLURAL][index] = updatedObject;
 
+      json = JSON.stringify(obj);
+      fs.writeFile(ENTITY_FILE_PATH, json, 'utf8', (err, res) => {
+        if(err){
+          console.error(err);
+        } else {
+          response.status(200).send(`${ENTITY_NAME} ${JSON.stringify(objectToUpdate)} has been successfully updated to ${JSON.stringify(updatedObject)}!`);
+        }
+      });
+    }
+  });
 });
 
-// UPDATE A SINGLE USER
-router.post('/:id', function (req, res) {
+// GETS ALL
+router.get('/', function (req, response) {
+  fs.readFile(ENTITY_FILE_PATH, 'utf8', (err, data) => {
+    if (err){
+        console.log(err);
+    } else {  
 
+      let obj = JSON.parse(data);
+      response.status(200).send(obj[ENTITY_NAME_PLURAL]);
+    }
+  });
 });
 
 module.exports = router;
